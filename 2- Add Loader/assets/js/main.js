35ad9124dcd0c7c2c31c46567ac3b177a8050f5a
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2023-03-27 22:31:48 +0200

    Add Loader

---
+++ 2- Add Loader/assets/js/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  const loader = document.getElementById("loader");
+  if(loader) {
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 1000);
+  }
+};
